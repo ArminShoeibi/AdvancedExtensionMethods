34ac098cf4fffb5a14bb7ab619901aed1e0702ee
Armin Shoeibi <a.shoeibi@alibaba.ir>
2021-07-23 21:50:52 +0430

    add more extension methods.

---
+++ AdvancedExtensionMethods.Patterns/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace AdvancedExtensionMethods.Patterns
+{
+    public static class ExtensionMethods
+    {
+        public static StringBuilder AL(this StringBuilder stringBuilder, string value)
+        {
+            return stringBuilder.AppendLine(value);
+        }
+
+        public static bool IsNullOrEmpty(this string str)
+        {
+            bool isNullOrEmpty = string.IsNullOrEmpty(str);
+            return isNullOrEmpty;
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(str);
+            return isNullOrWhiteSpace;
+        }
+
+        public static string Format(this string str, params object[] args)
+        {
+            string formattedString = string.Format(str, args);
+            return formattedString;
+        }
+
+    }
+}
---
+++ AdvancedExtensionMethods.Patterns/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace AdvancedExtensionMethods.Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringbuilder = new();
+            stringbuilder.AppendLine("First Line");
+            stringbuilder.AL("Second Line but with Shorthand of AppendLine");
+
+            string myName = "Armin";
+            bool isMyNameNullOrEmpty = myName.IsNullOrEmpty();
+            bool isMyNameNullOrWhiteSpace = myName.IsNullOrWhiteSpace();
+            Console.WriteLine($"Is My Name Null Or Empty? {isMyNameNullOrEmpty}");
+            Console.WriteLine($"Is My Name Null Or WhiteSpace? {isMyNameNullOrWhiteSpace}");
+
+            Console.WriteLine();
+
+            string emptyString = string.Empty;
+            Console.WriteLine($"Is provided string is Empty or NULL? {emptyString.IsNullOrEmpty()}");
+            Console.WriteLine($"Is provided string is Empty or NULL or WhiteSpace? {emptyString.IsNullOrWhiteSpace()}");
+
+            string nullString = default;
+            Console.WriteLine($"Is provided string is Empty or NULL? {nullString.IsNullOrEmpty()}");
+            Console.WriteLine($"Is provided string is Empty or NULL or WhiteSpace? {nullString.IsNullOrWhiteSpace()}");
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
