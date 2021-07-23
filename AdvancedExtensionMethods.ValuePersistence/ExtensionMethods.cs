af0f1c5f912f34b268c73452d0658f09d922aa6d
Armin Shoeibi <a.shoeibi@alibaba.ir>
2021-07-23 20:54:49 +0430

    Implement Extension Properties with Extension Methods.

---
+++ AdvancedExtensionMethods.ValuePersistence/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvancedExtensionMethods.ValuePersistence
+{
+    public static class ExtensionMethods
+    {
+        private static Dictionary<WeakReference, object> tagsDictionary = new();
+
+        public static object GetTag(this object obj)
+        {
+            WeakReference weakReferenceOfObject = tagsDictionary.Keys.FirstOrDefault(k => k.IsAlive && k.Target == obj);
+            return weakReferenceOfObject != null ? tagsDictionary[weakReferenceOfObject] : null;
+        }
+
+        public static void SetTag(this object obj, object tag)
+        {
+            WeakReference weakReferenceOfObject = tagsDictionary.Keys.FirstOrDefault(k => k.IsAlive && k.Target == obj);
+            if (weakReferenceOfObject is not null)
+            {
+                tagsDictionary[weakReferenceOfObject] = tag;
+            }
+            else
+            {
+                tagsDictionary[new WeakReference(obj)] = tag;
+            }
+        }
+
+    }
+}
---
+++ AdvancedExtensionMethods.ValuePersistence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdvancedExtensionMethods.ValuePersistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Armin";
+
+            myName.SetTag("Shoeibi");
+            object myNameTag = myName.GetTag();
+            Console.WriteLine(myNameTag);
+
+            myName.SetTag("Override old tag");
+            myNameTag = myName.GetTag();
+            Console.WriteLine(myNameTag);
+        }
+    }
+}
